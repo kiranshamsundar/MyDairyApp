a76125b45a5be13817867e88a946bf6f1b508845
Kiran Shamsundar <kiran.shamsundar@gmail.com>
2023-08-03 16:09:25 +0530

    Initial commit.

---
+++ core/util/src/main/java/com/example/util/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.util
+
+object Constants {
+
+    const val APP_ID="dairyapplication-zrvdt"
+    const val CLIENT_ID="383918466495-b4fadsve1636eih0o6g7pugbbduuth4r.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+
+    const val IMAGES_DATABASE = "images_db"
+    const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
+    const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
+}
